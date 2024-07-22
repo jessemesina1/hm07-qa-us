6cf0c53ac5b4d7ee35731b98338cffcaba0f0d8f
Jesse Mesina <jessemesina83@gmail.com>
2024-07-22 12:45:30 -0400

    Initial commit

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-922a7690-82e3-4290-b7f3-734108d970c2.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-e1cb6eb8-50bb-41dc-a573-4281427021d9.containerhub.tripleten-services.com"
     
 }
